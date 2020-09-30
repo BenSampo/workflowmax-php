5374070d618f2ae581b79ab85acd9bc34fe3c50d
Ben Sampson <ben@sampo.co.uk>
2020-09-30 18:05:24 +0100

    Improve type connector

--- src/Connector/TypeConnector.php
+++ src/Connector/TypeConnector.php
@@ -2,23 +2,27 @@
 
 namespace Sminnee\WorkflowMax\Connector;
 
-use Sminnee\WorkflowMax\ApiCall;
 use iter;
+use Sminnee\WorkflowMax\ApiCall;
 
 /**
- * Base class for all type-specific sub-connectors
+ * Base class for all type-specific sub-connectors.
  */
-class TypeConnector
+abstract class TypeConnector
 {
     /**
-     * Return a list of items from the given ApiCall
+     * Return a list of items from the given ApiCall.
      * @param  ApiCall $apiCall [description]
-     * @return [type]           [description]
+     * @return \Iterator
      */
-    function listFromApiCall(ApiCall $apiCall) {
+    public function listFromApiCall(ApiCall $apiCall)
+    {
         $self = $this;
-        return iter\map(function($record) use ($self) {
+
+        return iter\map(function ($record) use ($self) {
             return $self->byStub($record);
         }, $apiCall);
     }
+
+    abstract public function byStub($stubData);
 }
